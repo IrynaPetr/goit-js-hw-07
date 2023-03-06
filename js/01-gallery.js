e3ea8e97cf83db119425585bf0c481b51871532a
IrynaPetr <iryna.bieliakova@gmail.com>
2023-03-06 23:13:56 +0200

    fix

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryContainerEl = document.querySelector('div.gallery');
+
+function galleryCardsMarkup(items) {
+  return items.map(({preview, original, description}) => {
+    return `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+       <img class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}">
+            </a>
+   </div>`;
+  }).join('');
+
+};
+const cardsMarkupEl = galleryCardsMarkup(galleryItems);
+galleryContainerEl.insertAdjacentHTML('beforeend', cardsMarkupEl);
+
+galleryContainerEl.addEventListener('click', clickCardsGallery);
+function clickCardsGallery(e) {
+  e.preventDefault();
+  
+  if(e.target.nodeName !== "IMG") {
+    return
+  }
+  const descriotionModalImage = e.target.alt;
+  console.log(descriotionModalImage);
+  const modalWidowEl = e.target.dataset.source;
+const instance = basicLightbox.create(`<img src="${modalWidowEl}" alt="${descriotionModalImage}" width="800" heigth="600">`, {
+  onShow: (instance) => window.addEventListener("keydown", onEscape),
+  onClose: () => window.removeEventListener("keydown", onEscape),
+});
+  function onEscape(e) {
+    if(e.code === "Escape") {
+      instance.close()
+    }
+  }
+instance.show();
+}
+;
